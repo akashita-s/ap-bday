195c59449d3d4b29e6713c761b0e03c7a9c3ee5a
akashita-s <82257553+akashita-s@users.noreply.github.com>
2023-03-04 01:14:52 +0530

    head tag

--- Components/HeadTag.js
+++ Components/HeadTag.js
@@ -5,11 +5,10 @@ import { userinfo } from '../Constants/userinfo'
 const HeadTag = ({page}) => {
     return (
         <Head>
-            <title>{`${userinfo.logoText} | ${page}`}</title>
-            <link rel="icon" href="/favicon.jpg" />
+            <title>{userinfo.logoText}</title>
+            <link rel="icon" href="/cap.png" />
             <meta name="title" content={userinfo.logoText} />
-            <meta name="description" content={userinfo.greeting.subtitle} />
-
+{/* 
             <meta property="og:type" content="website" />
             <meta property="og:title" content={userinfo.logoText} />
             <meta property="og:description" content={userinfo.greeting.subtitle} />
@@ -18,7 +17,7 @@ const HeadTag = ({page}) => {
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:title" content={userinfo.logoText} />
             <meta property="twitter:description" content={userinfo.greeting.subtitle} />
-            <meta property="twitter:image" content="" />
+            <meta property="twitter:image" content="" /> */}
         </Head>
     )
 }
--- Constants/projects.js
+++ Constants/projects.js
@@ -16,7 +16,7 @@ export const projects = [
     //leave the projectlink field blank if it the project has not been deployed, alerts will take care of it
     {
         name: 'Trips', //project name
-        description: 'First of all you\'re the perfect partner as we both like going on trips. We complete each other.', //project description
+        description: 'I cannot begin to express how much joy and excitement I feel when we travel together. Exploring new places and experiencing different cultures with you by my side is one of the greatest pleasures of my life. From the moment we step off the plane, I know that we are about to embark on an incredible adventure. Whether we are hiking through scenic landscapes, trying new foods, or simply wandering through the streets of a new city, I feel so grateful to be sharing these experiences with you. There is something truly special about exploring the world with the person you love. I love how we can discover new things together and create unforgettable memories that we will cherish for a lifetime. Thank you for being my travel companion, my partner in adventure, and my best friend. I cannot wait to see where our next journey takes us.', //project description
         githubLink: '', //github repo link
         projectLink: '', //deployed project link
         tech: ['Let\'s Travel the World'], //techstack of the project, it is advisavble to mention only upto 3 technologies here to maintain the ui
@@ -24,7 +24,7 @@ export const projects = [
     },
     {
         name: 'Kisses',
-        description: 'I love that we never get tired of kissing each other. Both are equally clingy.',
+        description: 'I adore the way your kisses make me feel. Every time your lips touch mine, I feel a rush of warmth and love that fills me from head to toe. Your kisses are like a sweet embrace, a tender reminder of the depth of our connection. I love the way you hold me close, your arms wrapped around me in a warm embrace. I feel so safe and loved in your arms, and I know that nothing can ever come between us. Your kisses are like a secret language, a way for us to express our love without saying a word. With each gentle touch of your lips, I feel our bond growing stronger and deeper. ',
         githubLink: '/',
         projectLink: '/',
         tech: ['Let\s kiss a million times, and then some more!'],
@@ -32,7 +32,7 @@ export const projects = [
     },
     {
         name: 'Everything',
-        description: 'I never thought I\'ll spend so much time with a person. But now I start missing you even when you\'re away for some time.',
+        description: 'One of the things I love most about our relationship is how we always manage to have fun, no matter what we\'re doing. Whether we\'re trying out a new restaurant, taking a dance class, or simply wandering through the city, every moment with you feels like an adventure. I love how we can embrace spontaneity and try new things without hesitation. With you by my side, I feel like anything is possible. I cherish the memories we\'ve created together, and I look forward to all the new experiences we have yet to discover.',
         githubLink: '/',
         projectLink: '/',
         tech: ['All of me loves all of you.'],
